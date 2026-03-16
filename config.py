9451aefa85615f50d641faee6ed0d3b08f234ce9
Sergay Gutuev <alexey.letov2001@gmail.com>
2026-03-16 23:34:52 +0300

    add payment token

--- config.py
+++ config.py
@@ -10,5 +10,8 @@ SUPPORT_URL = os.getenv("SUPPORT_URL")
 MARZBAN_URL = os.getenv("MARZBAN_URL") #url site
 MARZBAN_USERNAME = os.getenv("MARZBAN_USERNAME")
 MARZBAN_PASSWORD = os.getenv("MARZBAN_PASSWORD")
+
+PAYMENT_TOKEN = os.getenv("PAYMENT_TOKEN")
+
 DB_PATH = 'orders.db'
 PROXY_URL = 'http://127.0.0.1:10808'
