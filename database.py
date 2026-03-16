79b5ae272cf11a145e721efac596eb7493c42c80
Sergay Gutuev <alexey.letov2001@gmail.com>
2026-03-16 22:35:18 +0300

    add method get_db

--- database.py
+++ database.py
@@ -1,9 +1,9 @@
-from os.path import curdir
-
 import aiosqlite
 
+DB_PATH = 'orders.db'
+
 async def create_db():
-    async with aiosqlite.connect('orders.db') as db:
+    async with get_db() as db:
         await db.execute('''
             CREATE TABLE IF NOT EXISTS orders (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -26,6 +26,9 @@ async def create_db():
                 ''')
         await db.commit()
 
+async def get_db():
+    return aiosqlite.connect(DB_PATH)
+
 # async def check_trial(user_id):
 #     async with aiosqlite.connect('orders.db') as db:
 #         cursor = await db.execute(
@@ -43,7 +46,7 @@ async def create_db():
 #         await db.commit()
 
 async def check_notification(user_id):
-    async with aiosqlite.connect('orders.db') as db:
+    async with get_db() as db:
         cursor = await db.execute(
             'SELECT user_id FROM notifications WHERE notification = 0 AND user_id = ?',
             (user_id,)
@@ -51,7 +54,7 @@ async def check_notification(user_id):
         return await cursor.fetchone()
 
 async def set_notified(user_id, notification):
-    async with aiosqlite.connect('orders.db') as db:
+    async with get_db() as db:
         await db.execute(
             'INSERT OR REPLACE INTO notifications (user_id, notification) VALUES (?, ?)',
             (user_id, notification)
@@ -59,7 +62,7 @@ async def set_notified(user_id, notification):
         await db.commit()
 
 async def check_pending_order(user_id):
-    async with aiosqlite.connect('orders.db') as db:
+    async with get_db() as db:
         cursor = await db.execute(
             'SELECT id FROM orders WHERE user_id = ? AND status = "pending"',
             (user_id,)
@@ -67,7 +70,7 @@ async def check_pending_order(user_id):
         return await cursor.fetchone() # Вернет ID заказа или None
 
 async def check_extend_order(user_id):
-    async with aiosqlite.connect('orders.db') as db:
+    async with get_db() as db:
         cursor = await db.execute(
             'SELECT id FROM orders WHERE user_id = ? AND status = "extend"',
             (user_id,)
@@ -75,7 +78,7 @@ async def check_extend_order(user_id):
         return await cursor.fetchone() # Вернет ID заказа или None
 
 async def check_order(user_id, status):
-    async with aiosqlite.connect('orders.db') as db:
+    async with get_db() as db:
         cursor = await db.execute(
             'SELECT id FROM orders WHERE user_id = ? AND status = ?',
             (user_id, status)
@@ -83,7 +86,7 @@ async def check_order(user_id, status):
         return await cursor.fetchone() # Вернет ID заказа или None
 
 async def add_order(user_id, tariff, days):
-    async with aiosqlite.connect('orders.db') as db:
+    async with get_db() as db:
         await db.execute(
             'INSERT INTO orders (user_id, tariff, days) VALUES (?, ?, ?)',
             (user_id, tariff, days)
@@ -91,7 +94,7 @@ async def add_order(user_id, tariff, days):
         await db.commit()
 
 async def add_expire_order(user_id, tariff, days, status):
-    async with aiosqlite.connect('orders.db') as db:
+    async with get_db() as db:
         await db.execute(
             'INSERT INTO orders (user_id, tariff, days, status) VALUES (?, ?, ?, ?)',
             (user_id, tariff, days, status)
@@ -99,7 +102,7 @@ async def add_expire_order(user_id, tariff, days, status):
         await db.commit()
 
 async def db_delete_user(order_id):
-    async with aiosqlite.connect('orders.db') as db:
+    async with get_db() as db:
         await db.execute(
             'DELETE FROM orders WHERE id = ?',
             (order_id,)
@@ -107,7 +110,7 @@ async def db_delete_user(order_id):
         await db.commit()
 
 async def update_order_status(order_id: int, new_status: str):
-    async with aiosqlite.connect('orders.db') as db:
+    async with get_db() as db:
         await db.execute(
             'UPDATE orders SET status = ? WHERE id = ?',
             (new_status, order_id)
@@ -115,7 +118,7 @@ async def update_order_status(order_id: int, new_status: str):
         await db.commit()
 
 async def get_order(order_id):
-    async with aiosqlite.connect('orders.db') as db:
+    async with get_db() as db:
         cursor = await db.execute(
             'SELECT * FROM orders WHERE id = ?',
             (order_id,)
@@ -124,7 +127,7 @@ async def get_order(order_id):
 
 
 async def get_orders_list(status):
-    async with aiosqlite.connect('orders.db') as db:
+    async with get_db() as db:
         cursor = await db.execute(
             'SELECT id, user_id, tariff, days FROM orders WHERE status = ?',
             (status,)
