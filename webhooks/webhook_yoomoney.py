f3237c6b32e9292db51dc0d0d0a9af6bac76ac80
Sergay Gutuev <alexey.letov2001@gmail.com>
2026-03-25 21:12:09 +0300

    feat: Если пришла транзакция на вебхук, статус заказа меняется в бд на succeeded

--- webhooks/webhook_yoomoney.py
+++ webhooks/webhook_yoomoney.py
@@ -3,6 +3,8 @@ import logging
 import traceback
 
 from fastapi import FastAPI, Request, HTTPException
+
+from database.database import succeeded_transaction
 from services.payment import successful_payment
 from yookassa.domain.notification import WebhookNotification
 from bot_instance import bot
@@ -18,13 +20,14 @@ async def webhook(request: Request):
         logging.error(f"Webhook error: {e}\n{traceback.format_exc()}")
         raise HTTPException(status_code=400)
     try:
-        get_metadata = notification.object.metadata
-        user_id = int(get_metadata.get('user_id'))
-        tariff = get_metadata.get('tariff')
-        day = int(get_metadata.get('day'))
-
+        payment_id = notification.object.id
         if notification.event == 'payment.succeeded':
-            await successful_payment(bot, user_id, tariff, day)
+            success = await succeeded_transaction(payment_id)
+            if success:
+                user_id, tariff, day = success
+                await successful_payment(bot, user_id, tariff, day, payment_id)
+            else:
+                logging.error(f'Ошибка! Success == None!')
 
     except Exception as e:
         logging.error(f"Payment processing error: {e}\n{traceback.format_exc()}")
