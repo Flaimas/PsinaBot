bac89de6cfcbc344e83917edf748d2b0c94ab2c8
Sergay Gutuev <alexey.letov2001@gmail.com>
2026-04-10 18:29:31 +0300

    feat: добавил роутер админа

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from aiogram import Bot, Dispatcher
 from aiogram.types import BotCommand, BotCommandScopeDefault
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from services.scheduler import check_vpn_expire, check_expire_users
-from handlers import start, help, submenu, instructions, referral, payment, tariff_menu
+from handlers import start, help, submenu, instructions, referral, payment, tariff_menu, admin
 from database.database import create_db
 from config import UVICORN_IP, UVICORN_PORT, SSL_KEY, SSL_CER
 from services.marzban import marzban_api
@@ -20,6 +20,7 @@ dp.include_router(instructions.router)
 dp.include_router(referral.router)
 dp.include_router(payment.router)
 dp.include_router(tariff_menu.router)
+dp.include_router(admin.router)
 
 async def set_commands(bot_: Bot):
     commands = [
